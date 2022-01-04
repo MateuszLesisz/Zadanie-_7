b985803d116f21b6b33283326b18df7d73dcdafe
Mateusz Lesisz <m.lesisz97@wp.pl>
2022-01-04 13:51:14 +0100

    Start project

---
+++ Ex  7/Ex-7/src/test/java/com/infoshareacademy/Ex7/Ex7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infoshareacademy.Ex7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
