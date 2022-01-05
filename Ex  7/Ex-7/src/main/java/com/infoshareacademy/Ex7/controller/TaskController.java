7d176d6a948e669f4315e6c15e77fd31c9121ea0
Mateusz Lesisz <m.lesisz97@wp.pl>
2022-01-05 12:04:56 +0100

    Add messages to task

--- Ex  7/Ex-7/src/main/java/com/infoshareacademy/Ex7/controller/TaskController.java
+++ Ex  7/Ex-7/src/main/java/com/infoshareacademy/Ex7/controller/TaskController.java
@@ -1,13 +1,24 @@
 package com.infoshareacademy.Ex7.controller;
 
+import com.infoshareacademy.Ex7.service.TaskService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class TaskController {
 
-    @GetMapping
-public String getTask() {
-    return "task";
-}
-}
+        private TaskService taskService;
+
+        public TaskController(TaskService taskService) {
+            this.taskService = taskService;
+        }
+
+    @GetMapping("/addNewTasks/{id}")
+        public String getNewTask(@PathVariable Long id, Model model) {
+            model.addAttribute("task",taskService.find(id));
+            return "task";
+        }
+    }
+
