c7afab680fb23f5694957a4872a1fa7593cbd14c
Mateusz Lesisz <m.lesisz97@wp.pl>
2022-01-10 11:51:20 +0100

    Add form

--- Ex  7/Ex-7/src/main/java/com/infoshareacademy/Ex7/controller/FormController.java
+++ Ex  7/Ex-7/src/main/java/com/infoshareacademy/Ex7/controller/FormController.java
@@ -1,13 +1,34 @@
 package com.infoshareacademy.Ex7.controller;
 
+import com.infoshareacademy.Ex7.dto.TaskDto;
+import com.infoshareacademy.Ex7.service.TaskService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class FormController {
+    private final TaskService taskService;
 
-    @GetMapping("addNewTask")
-    public String getNewTask() {
-        return "form";
+    public FormController(TaskService taskService) {
+        this.taskService = taskService;
     }
+
+
+    @GetMapping("tasks/new")
+    public String getTaskForm(Model model) {
+        model.addAttribute("task", new TaskDto());
+        return "form";
+
+    }
+
+    @PostMapping("tasks/new")
+    public String sendTask(@ModelAttribute("task") TaskDto task) {
+        taskService.create(task);
+        return "form-succes";
+    }
+
+
 }
