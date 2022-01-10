d896662bf20d12b012727d2526091f39d445978c
Mateusz Lesisz <m.lesisz97@wp.pl>
2022-01-10 15:50:10 +0100

    tactical commit

--- Ex  7/Ex-7/src/main/java/com/infoshareacademy/Ex7/repository/TaskRepository.java
+++ Ex  7/Ex-7/src/main/java/com/infoshareacademy/Ex7/repository/TaskRepository.java
@@ -10,33 +10,10 @@ import java.util.concurrent.atomic.AtomicLong;
 @Repository
 public class TaskRepository extends FakeRepository<Long, Task> {
 
-    private static final int WEEKEND = 2;
     private final AtomicLong nextId = new AtomicLong(1L);
 
     @PostConstruct
     public void initDb() {
-
-        Task threads = new Task();
-        threads.setDescription("threads");
-        threads.setDueDate(LocalDate.now().plusDays(WEEKEND));
-        threads.setCategory("asfas");
-        threads.setPriority(12);
-        save(threads);
-
-        Task task2 = new Task();
-        task2.setDescription("task2");
-        task2.setDueDate(LocalDate.now());
-        task2.setCategory("12321");
-        task2.setPriority(14);
-        save(task2);
-
-        Task task3 = new Task();
-        task3.setDescription("task3");
-        task3.setDueDate(LocalDate.now());
-        task3.setCategory("12321");
-        task3.setPriority(14234);
-        save(task3);
-
     }
 
     @Override
